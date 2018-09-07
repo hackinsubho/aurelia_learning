29d24272656793a0ee78a1a9905c2a4b55b24810
hackinsubho <subho3010@gmail.com>
2018-09-07 19:59:35 +0530

    Converters
    Currency Converters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 export class App {
   constructor() {
      this.currentDate = new Date();
+     this.update();
+     setInterval(() => this.update(), 1000);
   }
+  update() {
+    this.myCurrency = Math.random() * 1000;
+ }
 }
--- src/converters.js
+++ src/converters.js
@@ -1,7 +1,14 @@
 import moment from 'moment';
+import numeral from 'numeral';
 
 export class DateFormatValueConverter {
    toView(value) {
       return moment(value).format('D/M/YYYY');
    }
+}
+
+export class CurrencyFormatValueConverter {
+  toView(value) {
+     return numeral(value).format('($0,0.00)');
+  }
 }
