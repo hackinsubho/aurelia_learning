f0ac824a675cdb116d62ed7867861d190de1f3d3
hackinsubho <subho3010@gmail.com>
2018-09-07 20:50:56 +0530

    Forms
    Checkbox and radio button

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 export class App {
   email = '';
    password = '';
+   options = [];
+   selectedOption = {};
 
    signup() {
       var myUser = { email: this.email, password: this.password }
       console.log(myUser);
    };
+   constructor() {
+    this.options = [
+      {id:1, text:'First'}, 
+      {id:2, text:'Second'}, 
+      {id:3, text:'Third'}
+   ]; 
+   this.selectedOption = this.options[0];
+    this.isChecked = false;
+ }
+ submit() {
+  console.log("isChecked: " + this.isChecked);
+  console.log('checked: ' + this.selectedOption.id);
+}
 }
