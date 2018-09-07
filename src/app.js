a5c715bed964d900e9a5c6ece2ff31e0f6ed03cc
hackinsubho <subho3010@gmail.com>
2018-09-07 18:32:43 +0530

    Dependency test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+// export class App {
+//   message = 'Welcome to Aurelia!';
+// }
+import {inject} from 'aurelia-framework';
+import {DependencyTest} from './dependency-test';
+
+@inject(DependencyTest)
+
 export class App {
-  message = 'Welcome to Aurelia!';
+   constructor(DependencyTest) {
+      console.log(DependencyTest);
+   }
 }
