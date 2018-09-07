7555f9a76bfe3594d051037e3906fb3277ffbf37
hackinsubho <subho3010@gmail.com>
2018-09-07 20:40:23 +0530

    Event Aggregator
    Aurelia event aggregator has three methods. The publish method will fire off events and can be used by multiple subscribers. For subscribing to an event, we can use the subscribe method. And finally, we can use the dispose method to detach the subscribers. The following example demonstrates this.
    We need to click the SUBSCRIBE button to listen for data that will be published in future. Once the subscriber is attached, whenever new data is sent, the console will log it. If we click the PUBLISH button five times, we will see that it is logged every time.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class App {
-  myFunction() {
-     console.log('The function is triggered...');
-  }
+   constructor(eventAggregator) {
+      this.eventAggregator = eventAggregator;
+   }
+
+   publish() {
+      var payload = 'This is some data...';
+      this.eventAggregator.publish('myEventName', payload);
+   }
+
+   subscribe() {
+      this.subscriber = this.eventAggregator.subscribe('myEventName', payload => {
+         console.log(payload);
+      });
+   }
+
+   dispose() {
+      this.subscriber.dispose();
+      console.log('Disposed!!!');
+   }
 }
