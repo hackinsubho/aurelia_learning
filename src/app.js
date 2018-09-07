51048f324b58b5468e0cec8ed2752512682b7946
hackinsubho <subho3010@gmail.com>
2018-09-07 21:42:20 +0530

    HTTP requests in Aurelia framework
    We are creating functions for every requests we need (GET, POST, PUT and DELETE).
    We can run the app and click GET, POST, PUT and DEL buttons, respectively. We can see in the console that every request is successful, and the result is logged.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,56 @@
+// import 'fetch';
+import {HttpClient, json} from 'aurelia-fetch-client';
+
+let httpClient = new HttpClient();
+
 export class App {
   message = 'Welcome to Aurelia!';
+
+  getData() {
+    httpClient.fetch('http://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(data => {
+       console.log(data);
+    });
+ }
+  myPostData = { 
+    id: 101
+ }
+
+ postData(myPostData) {
+    httpClient.fetch('http://jsonplaceholder.typicode.com/posts', {
+       method: "POST",
+       body: JSON.stringify(myPostData)
+    })
+  
+    .then(response => response.json())
+    .then(data => {
+       console.log(data);
+    });
+ }
+myUpdateData = {
+      id: 1
+   }
+	
+   updateData(myUpdateData) {
+      httpClient.fetch('http://jsonplaceholder.typicode.com/posts/1', {
+         method: "PUT",
+         body: JSON.stringify(myUpdateData)
+      })
+		
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+      });
+   }
+
+   deleteData() {
+      httpClient.fetch('http://jsonplaceholder.typicode.com/posts/1', {
+         method: "DELETE"
+      })
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+      });
+   }
 }
